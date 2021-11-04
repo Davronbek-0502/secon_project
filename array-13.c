dd26b7b5e4f486fcaa75cdbc1e1ad6dad2b6b00a
Your Name <youremail@domain.com>
2021-11-04 20:35:43 +0500

    13-misol

---
+++ array-13.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+int main (){
+
+    int n=1, massiv[] = {2,3,4,5,6,7,8,9,7,7,8,6,7,};
+
+    for (int i=n;i<13;i+=2){
+        printf("massiv[%d]\n",i);
+       
+    }
+
+
+    return 0;
+}
